e488694db617af4929783e1dc84ad2fd648d0be3
t0933095470 <thethao165@gmail.com>
2022-09-12 09:13:00 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Input two value into the program
+        Console.WriteLine("Input value a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input value b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        
+        //Create some calculations
+        int c = a + b;
+        Console.WriteLine("Sum of a and b is equal to: " + c);
+        int d = a - b;
+        Console.WriteLine("Subtract of a and b is equal to: " + d);
+        int e = a * b;
+        Console.WriteLine("Multiplication of a and b is: " + e);
+        int f = a / b;
+        Console.WriteLine("Division of a to b is: " + f);
+        int g = a % b;
+        Console.WriteLine("Modulus between a and b is: " + g);
+
+    }
+}
